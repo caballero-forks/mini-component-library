44059ec3b7801c8371b44ef0ae2c578f6f9aa3d8
Luis <mendoza386@gmail.com>
2022-06-19 22:01:39 +0200

    first steps for input with icon

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,44 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return (
+    <Wrapper>
+      <Icon id="search" size={20} strokeWidth={1}></Icon>
+      <Input 
+        type="text" 
+        id="name" 
+        name="name"
+        required
+        minlength="4"
+        maxlength="8" 
+        size="10"
+        placeholder={placeholder}
+      >   
+      </Input>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex; 
+  align-items: center; 
+  width: 270px;
+  
+`;
+
+const Input = styled.input`
+  padding: 4px;
+  padding-left: 8px; 
+  border: none;
+  font-family: "Roboto", "sans-serif";
+  font-size: 14px;
+  border-bottom: 1px solid ${COLORS.black};
+  
+  &[type=text] {
+    color: ${COLORS.black};
+    font-weight: 700;
+  }
+`;
+
 
 export default IconInput;
