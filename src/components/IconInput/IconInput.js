40ae63ddfa1a5e713a93ddb5c8d9ade38fe0baca
Luis <mendoza386@gmail.com>
2022-06-27 16:36:05 +0200

    final features added

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,19 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    "--font-size": 14 + "px",
+    "--line-height": 16 + "px",
+    "--border-thick": 1 + "px",
+  },
+  large: {
+    "--font-size": 18 + "px",
+    "--line-height": 21 + "px",
+    "--border-thick": 2 + "px",
+  }
+};
+
 const IconInput = ({
   label,
   icon,
@@ -13,10 +26,15 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
+  const styles = SIZES[size];
+
   return (
     <Wrapper>
-      <Icon id="search" size={20} strokeWidth={1}></Icon>
-      <Input 
+      <VisuallyHidden><label for={label}>{label}</label></VisuallyHidden>
+      <IconWrapper>
+        <Icon id={icon} size={size === "small" ? 16 : 20} strokeWidth={size === "small" ? 1 : 2}></Icon>
+      </IconWrapper>
+      <TextInput  
         type="text" 
         id="name" 
         name="name"
@@ -25,31 +43,67 @@ const IconInput = ({
         maxlength="8" 
         size="10"
         placeholder={placeholder}
+        style={styles}
+        width= {width}
+        icon={icon}
       >   
-      </Input>
+       
+      </TextInput>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div`
-  display: flex; 
-  align-items: center; 
-  width: 270px;
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  /* display: flex; 
+  align-items: center;  */
+  color: ${COLORS.gray700};
   
+  &:hover {
+    color: ${COLORS.black};
+  }
 `;
 
-const Input = styled.input`
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center; 
+  /* margin: auto 0; */
+  /* height: var(--size); */
+`;
+
+const TextInput = styled.input`
+  width: ${props => props.width + "px"};
   padding: 4px;
-  padding-left: 8px; 
+  padding-left: 24px;
   border: none;
   font-family: "Roboto", "sans-serif";
-  font-size: 14px;
-  border-bottom: 1px solid ${COLORS.black};
+  font-size: var(--font-size);
+  line-height: var(--line-height);
+  border-bottom: var(--border-thick) solid ${COLORS.black};
   
   &[type=text] {
-    color: ${COLORS.black};
+    color: inherit;
     font-weight: 700;
+
+    &:hover {
+      color: ${COLORS.black};
   }
+  }
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+
+  &:focus {
+    outline-offset: 4px;
+  }
+
+  
 `;
 
 
